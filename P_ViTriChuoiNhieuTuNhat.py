b88ceadf855cd3b81001cd0c50a996fb6b9737d2
TamNhien <canhthien91@gmail.com>
2024-03-28 21:24:48 +0700

    Add project files.

---
+++ P_ViTriChuoiNhieuTuNhat.py
@@ -0,0 +1,7 @@
+﻿L = input("Nhập vào danh sách các chuỗi, cách nhau bởi dấu phẩy: ").split(',')
+max_word_count, max_word_index = max(((len(string.split()), i) for i, string in enumerate(L)), default=(0, -1))
+
+if max_word_index != -1:
+    print(f"Chuỗi có nhiều từ nhất ở vị trí {max_word_index}: {L[max_word_index]}")
+else:
+    print("Danh sách không có chuỗi nào.")
